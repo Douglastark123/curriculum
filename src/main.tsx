1a40e22310e6765d5980bc8d60ffd33aa5d984aa
Douglas A. da Silva <douglastark123@gmail.com>
2023-09-15 18:14:38 -0400

    deploy on github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { NextUIProvider } from '@nextui-org/react'
 import App from './App.tsx'
-import './index.css'
+import './global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider className="h-full">
+      <main className="h-full bg-background text-foreground dark">
+        <App />
+      </main>
+    </NextUIProvider>
   </React.StrictMode>,
 )
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
+import { nextui } from '@nextui-org/react'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        night: '#141414',
+        cardBlack: '#18181b',
+        antiFlashWhite: '#eef0f2',
+        zaffre: '#0d21a1',
+        oxfordBlue: '#011638',
+      },
+    },
   },
-  plugins: [],
+  darkMode: 'class',
+  plugins: [nextui()],
 }
